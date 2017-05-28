bdc1fec503c087004a58cbf5f780ed8fa6366647
Nathan Kasco <nkasco@gmail.com>
2017-05-27 23:24:45 -0400

    Added contact form

--- public/contact.js
+++ public/contact.js
@@ -1,5 +1,15 @@
 window.addEventListener('load', 
     function() {
+        var btnSubmit = document.getElementById('btnSubmit');
+
+        btnSubmit.addEventListener('click', e => {
+            var name = document.getElementById('contactName').value;
+            var email = document.getElementById('contactEmail').value;
+            var message = document.getElementById('contactMessage').value;
+
+            alert(name + email + message);
+        });
+
         firebase.auth().onAuthStateChanged(firebaseUser => {
             var username = document.getElementById('navTxt1');
             
