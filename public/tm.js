33378806027f0a0c1dc638b05d9a86c98a192f42
Nathan Kasco <nkasco@gmail.com>
2017-05-29 10:45:33 -0400

    Added resize event listener

--- public/tm.js
+++ public/tm.js
@@ -89,3 +89,18 @@ function loginUserLink(){
         }
     });
 }
+
+var addResizeEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
+
+addResizeEvent(window, "resize", function(event) {
+  //TODO: Add conditions if size is less than X
+});
